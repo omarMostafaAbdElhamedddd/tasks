3e8561ff61326d3f6d8e3ca33d30dbabc1474ec3
youssef <youssef1739@acic.edu.eg>
2026-02-07 15:15:26 +0200

    add task

--- lib/addTask/addTaskScreen.dart
+++ lib/addTask/addTaskScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/helper/color.dart';
+import 'package:tasks/helper/customWidgets/customTextFormField.dart';
+import 'package:tasks/helper/customWidgets/customButton.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({super.key});
@@ -10,10 +12,54 @@ class AddTaskScreen extends StatelessWidget {
       backgroundColor: AppColors.whiteColor,
       appBar: AppBar(
         backgroundColor: AppColors.whiteColor,
-        title: Text("Add task",style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w500
-        ),),
+        elevation: 0,
+        title: Text(
+          "Add task",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            CustomTextFormField(
+              hintText: "Task name",
+            ),
+
+            const SizedBox(height: 16),
+
+            CustomTextFormField(
+              hintText: "Description",
+            ),
+
+            const SizedBox(height: 16),
+
+            GestureDetector(
+              onTap: () async {
+                await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2100),
+                );
+              },
+              child: AbsorbPointer(
+                child: CustomTextFormField(
+                  hintText: "Due date",
+                ),
+              ),
+            ),
+
+            const Spacer(),
+
+            CustomButton(
+              text: "Add Task",
+            ),
+          ],
+        ),
       ),
     );
   }
