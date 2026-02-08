988666a23406276e49c967e4786095daa7888f32
youssef <youssef1739@acic.edu.eg>
2026-02-08 18:25:59 +0200

    add task

--- lib/notofications/notoficationScreen.dart
+++ lib/notofications/notoficationScreen.dart
@@ -13,7 +13,24 @@ class NotoficationScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         title: Text("Notoifcations"),
       ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+         return  NotoficationItem ();
+        },
+    ));
+  }
+}
+class NotoficationItem extends StatelessWidget {
+  const NotoficationItem({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Notofication title'),
+        Text('your duedate for this task is on 4:15'),
+        Text('9\2\2026 4:12')
+      ],
     );
   }
 }
