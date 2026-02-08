79b68d3b25befce362268e593510a03a6d2b30af
Mahmoud <mahmoudelshawy2010@gmail.com>
2026-02-07 18:50:02 -0800

    home

--- lib/profile/profileScreen.dart
+++ lib/profile/profileScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasks/helper/color.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -14,6 +15,71 @@ class ProfileScreen extends StatelessWidget {
           fontSize: 16,
           fontWeight: FontWeight.w500
         ),),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(20), 
+              ),
+              child: const Icon(
+                Icons.person,
+                size: 60,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Mahmoud ",
+                  style: TextStyle(
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "mahmoudelshawy2010@gmail.com ",
+                  style: TextStyle(
+                    fontSize: 14, 
+                    color: Colors.grey
+                  ),
+                ),
+                ],
+            ),
+          ),
+          Text(
+                  "change your username",
+                  style: TextStyle(
+                    fontSize: 14, 
+                    color: Colors.blue
+                  ),
+                ),
+                Text(
+                  "change your pasword",
+                  style: TextStyle(
+                    fontSize: 12, 
+                    color: Colors.red
+                  ),
+                ),
+              
+        ],
       ),
     );
   }
