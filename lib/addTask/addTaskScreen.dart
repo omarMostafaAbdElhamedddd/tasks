d0e49fa868a2e9ff0d39e098c9ba8eeccbb377df
Mahmoud <mahmoudelshawy2010@gmail.com>
2026-02-11 11:47:59 -0800

    Home

--- lib/addTask/addTaskScreen.dart
+++ lib/addTask/addTaskScreen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tasks/helper/color.dart';
 import 'package:tasks/helper/customWidgets/customTextFormField.dart';
 import 'package:tasks/helper/customWidgets/customButton.dart';
 
-class AddTaskScreen extends StatelessWidget {
+class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
 
   @override
+  State<AddTaskScreen> createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  @override
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
@@ -23,42 +30,69 @@ class AddTaskScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            CustomTextFormField(
-              hintText: "Task name",
-            ),
-
-            const SizedBox(height: 16),
-
-            CustomTextFormField(
-              hintText: "Description",
-            ),
-
-            const SizedBox(height: 16),
-
-            GestureDetector(
-              onTap: () async {
-                await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                );
-              },
-              child: AbsorbPointer(
-                child: CustomTextFormField(
-                  hintText: "Due date",
+        child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          key: formKey,
+          child: Column(
+            children: [
+              CustomTextFormField(
+                hintText: "Task name",
+                validator: (data){
+                              if(data!.isEmpty){
+                                return "Please enter your name";
+                              }
+                              return null;
+                },
+              ),
+          
+              const SizedBox(height: 16),
+          
+              CustomTextFormField(
+                hintText: "Description",
+                validator: (data){
+                              if(data!.isEmpty){
+                                return "Please enter your description";
+                              }
+                              return null;
+                },
+              ),
+          
+              const SizedBox(height: 16),
+          
+              GestureDetector(
+                onTap: () async {
+                  await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                },
+                child: AbsorbPointer(
+                  child: CustomTextFormField(
+                    hintText: "Due date",
+                    validator: (data){
+                              if(data!.isEmpty){
+                                return "Please enter your due date";
+                              }
+                              return null;
+                },
+                  ),
                 ),
               ),
-            ),
+          
+              const Spacer(),
+          
+              CustomButton(
+                onTap: (){
+                  if(formKey.currentState!.validate()){
 
-            const Spacer(),
-
-            CustomButton(
-              text: "Add Task",
-            ),
-          ],
+                  }
+                },
+                text: "Add Task",
+              ),
+            ],
+          ),
         ),
       ),
     );
